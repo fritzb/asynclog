0f1d6238ba1013dd82fd0cd2128beda546a04f41
Fritz Budiyanto <fritzbx@icloud.com>
2019-03-03 20:05:38 -0800

    Refactor string formater

--- src/lib/asynclog.cpp
+++ src/lib/asynclog.cpp
@@ -31,6 +31,7 @@ limitations under the License.
 
 #include "asynclog.h"
 #include "ringbuffer.h"
+#include "stringformat.h"
 
 
 
@@ -52,116 +53,6 @@ uint32_t Log::allocateId() {
         globalId_++;
     }
     return id;
-}
-
-// Write u8 into the non circular buffer
-uint32_t Log::memSetByte(char *s, uint8_t u8) {
-    *(uint8_t *)s = u8;
-    return sizeof(u8);
-}
-
-//Write u16 into the circular buffer
-uint32_t Log::memSetWord(char *s, uint16_t u16) {
-    memcpy(s, &u16, sizeof(u16));
-    return sizeof(u16);
-}
-
-// Write u32 into the circular buffer
-uint32_t Log::memSetInt(char *s, uint32_t u32) {
-    memcpy(s, &u32, sizeof(u32));
-    return sizeof(u32);
-}
-
-uint32_t Log::memSetLong64(char *s, uint64_t u64) {
-    memcpy(s, &u64, sizeof(u64));
-    return sizeof(u64);
-}
-
-// Write double into the circular buffer
-uint32_t Log::memSetDouble(char *s, double d) {
-    memcpy(s, &d, sizeof(d));
-    return sizeof(d);
-}
-
-uint32_t Log::memSetPtr(char *s, void *src) {
-    memcpy(s, &src, sizeof(void *));
-    return sizeof(void *);
-}
-
-uint32_t Log::memSetString(char *s, const char *src) {
-    const char *NULL_STRING = "(null)";
-    if (!src) {
-        strcpy(s, NULL_STRING);
-        return strlen(NULL_STRING) + 1;
-    }
-
-    uint32_t str_size = strlen(src);
-    memcpy(s, src, str_size + 1);
-    return str_size + 1;
-}
-
-// Read from memory API
-uint8_t Log::memGetByte(uint8_t *buf, uint32_t dstIndex) {
-    uint8_t u8;
-    memcpy(&u8, (buf + dstIndex), sizeof(u8));
-    return u8;
-}
-
-uint16_t Log::memGetWord(uint8_t *buf, uint32_t dstIndex) {
-    uint16_t u16;
-    memcpy(&u16, (buf + dstIndex), sizeof(u16));
-    return u16;
-}
-
-uint32_t Log::memGetInt(uint8_t *buf, uint32_t dstIndex) {
-    uint32_t u32;
-    memcpy(&u32, (buf + dstIndex), sizeof(u32));
-    return u32;
-}
-
-double Log::memGetDouble(uint8_t *buf, uint32_t dstIndex) {
-    double d;
-    memcpy(&d, (buf + dstIndex), sizeof(d));
-    return d;
-}
-
-void * Log::memGetPtr(uint8_t *buf, uint32_t dstIndex) {
-    void *ptr;
-    memcpy(&ptr, (buf + dstIndex), sizeof(ptr));
-    return ptr;
-}
-
-uint64_t Log::memGetLong64(uint8_t *buf, uint32_t dstIndex) {
-    uint64_t u64;
-    memcpy(&u64, (buf + dstIndex), sizeof(u64));
-    return u64;
-}
-
-uint32_t Log::memGetString(uint8_t *buf, uint32_t bufferIndex, char *dst) {
-    uint32_t di = 0;
-
-    while (buf[bufferIndex] != 0) {
-        dst[di] = buf[bufferIndex];
-        di++;
-        bufferIndex++;
-    }
-    return di;
-}
-
-// Get string from buffer (excluding the zero terminated character)
-uint32_t Log::memGetString(uint8_t *buf, uint32_t bufferIndex, char *dst, uint32_t maxStringLength) {
-    uint32_t di = 0;
-
-    while (buf[bufferIndex] != 0) {
-        if ((di+1) > maxStringLength) {
-            getStringCorruptedCount++;
-            return di;
-        }
-        dst[di] = buf[bufferIndex];
-        di++;
-        bufferIndex++;
-    }
-    return di;
 }
 
 uint32_t Log::setHeader(char *dst,
@@ -227,137 +118,8 @@ void Log::traceVargs(bool with_ts, const char *function_name, uint32_t line_numb
     char *dst = buffer;
 
     va_start(va, s);
-
     dst += sizeof(Log::Header);
-
-    while (s[i] != 0) {
-        // PERCENT
-        if (s[i] == '%') {
-            if (insidePercent) {
-                insidePercent = false;
-                goto next;
-            }
-            insidePercent = true;
-            goto next;
-        }
-
-        if (insidePercent) {
-            insidePercent = false;
-
-            switch (s[i]) {
-                case 'h':
-                    switch (s[i+1]) {
-                        case 'o':
-                        case 'x':
-                        case 'X':
-                        case 'u':
-                            u16 = va_arg(va, unsigned int);
-                            dst += memSetWord(dst, u16);
-                            break;
-
-                        case 'd':
-                        case 'i':
-                            u16 = va_arg(va, int);
-                            dst += memSetWord(dst, u16);
-                            break;
-
-                        case 'h':
-                            if ((s[i+2] == 'x') ||
-                                (s[i+2] == 'X') ||
-                                (s[i+2] == 'u') ||
-                                (s[i+2] == 'o')) {
-                                u16 = va_arg(va, unsigned int);
-                                dst += memSetWord(dst, u16);
-                                continue;
-                            }
-                            break;
-                    }
-                    break;
-
-                case '%':
-                    break;
-
-                case 'c':
-                    u8 = va_arg(va, int);
-                    dst += memSetByte(dst, u8);
-                    break;
-
-                case 'd':
-                case 'u':
-                case 'i':
-                case 'x':
-                case 'X':
-                    u32 = va_arg(va, unsigned int);
-                    dst += memSetInt(dst, u32);
-                    break;
-
-                case 'f':
-                    d = va_arg(va, double);
-                    dst += memSetDouble(dst, d);
-                    break;
-
-                case 'p':
-                    ptr = va_arg(va, void *);
-                    dst += memSetPtr(dst, ptr);
-                    break;
-
-                case 's':
-                    ptr = va_arg(va, char *);
-                    dst += memSetString(dst, (const char *) ptr);
-                    break;
-
-                case 'l':
-                    switch (s[i+1]) {
-                        case 'l':
-                            if ((s[i+2] == 'x') ||
-                                (s[i+2] == 'X') ||
-                                (s[i+2] == 'd') ||
-                                (s[i+2] == 'i') ||
-                                (s[i+2] == 'u')) {
-                                i += 2;
-                                u64 = va_arg(va, long long);
-                                dst += memSetLong64(dst, u64);
-                                continue;
-                            }
-                            break;
-
-                        case 'x':
-                        case 'X':
-                        case 'd':
-                        case 'i':
-                        case 'u':
-                            u32 = va_arg(va, unsigned int);
-                            dst += memSetInt(dst, u32);
-                            break;
-
-                        case 'f':
-                            d = va_arg(va, double);
-                            dst += memSetDouble(dst, d);
-                            break;
-
-                        default:
-                            // Don't know, could be NULL terminated symbol
-                            break;
-                    }
-                    break;
-
-                default:
-                    if (s[i] == '-' || s[i] == '+' || s[i] == ' ' || s[i] == '#' ||
-                        s[i] == '.' || (isdigit(s[i]))) {
-                        insidePercent = true;
-                        goto next;
-                    }
-
-                    //printf("Unhandled symbol '%c', format: %s", s[i], s);
-                    assert(0);
-                    insidePercent = false;
-                    goto next;
-            }
-        }
-
-        next:
-        i++;
-    }
+    stringFormat_->parseStringFormat(dst, s, va);
     va_end(va);
 
     // Copy the temporary buffer
@@ -655,217 +417,17 @@ int Log::printAtIndex(uint32_t index, char *dst, uint32_t *next_index,
     i = 0;
     s = &hdr->format[0];
     start_buf = (uint8_t *)hdr;
-    while (s[i] != 0) {
-        // PERCENT?
-        eat = false;
-        start = i;
-        if (s[i] == '%') {
-            i++;
 
-            // %#x or %-2.2d or %+2.2x
-            if (s[i] && (s[i] == '0' || s[i] == ' ' || s[i] == '#' ||
-                         s[i] == '-' || s[i] == '+')) {
-                i++;
-            }
-
-            // %2.2x
-            while (s[i] != 0) {
-                if (isdigit(s[i])) {
-                    i++;
-                } else {
-                    break;
-                }
-            }
-            if (s[i] && s[i] == '.') {
-                i++;
-            }
-            while (s[i] != 0) {
-                if (isdigit(s[i])) {
-                    i++;
-                } else {
-                    break;
-                }
-            }
-            if (s[i]) {
-                switch (s[i]) {
-                    case 'h':
-                        i++;
-                        switch(s[i]) {
-                            case 'd':
-                            case 'u':
-                            case 'x':
-                            case 'X':
-                            case 'o':
-                            case 'i':
-                                i++;
-                                strncpy(format, &s[start], i-start+1);
-                                u16 = memGetWord(start_buf, buf_index);
-                                buf_index = indexInc(buf_index, sizeof(u16));
-                                dst += sprintf(dst, format, u16);
-                                eat = true;
-                                break;
-                            case 'h':
-                                i++;
-                                switch(s[i]) {
-                                    case 'x':
-                                    case 'X':
-                                    case 'u':
-                                    case 'o':
-                                        i++;
-                                        strncpy(format, &s[start], i-start+1);
-                                        u16 = memGetWord(start_buf, buf_index);
-                                        buf_index = indexInc(buf_index, sizeof(u16));
-                                        dst += sprintf(dst, format, u16);
-                                        eat = true;
-                                        break;
-                                }
-                        }
-                        break;
-
-                    case 'c':
-                        i++;
-                        strncpy(format, &s[start], i-start+1);
-                        u8 = memGetByte(start_buf, buf_index);
-                        buf_index = indexInc(buf_index, sizeof(u8));
-                        dst += sprintf(dst, format, u8);
-                        eat = true;
-                        break;
-
-                    case 'd':
-                    case 'u':
-                    case 'i':
-                    case 'x':
-                    case 'X':
-                        i++;
-                        strncpy(format, &s[start], i-start+1);
-                        u32 = memGetInt(start_buf, buf_index);
-                        buf_index = indexInc(buf_index, sizeof(u32));
-                        dst += sprintf(dst, format, u32);
-                        eat = true;
-                        break;
-
-                    case 'f':
-                        i++;
-                        strncpy(format, &s[start], i-start+1);
-                        d = memGetDouble(start_buf, buf_index);
-                        buf_index = indexInc(buf_index, sizeof(d));
-                        dst += sprintf(dst, format, d);
-                        eat = true;
-                        break;
-
-                    case 'p':
-                        i++;
-                        strncpy(format, &s[start], i-start+1);
-                        ptr = (void *)memGetPtr(start_buf, buf_index);
-                        buf_index = indexInc(buf_index, sizeof(void *));
-                        dst += sprintf(dst, format, ptr);
-                        eat = true;
-                        break;
-
-                        // string
-                    case 's':
-                        i++;
-                        strncpy(format, &s[start], i-start+1);
-                        //printf("%s\n", format);
-
-                        //string_len = getString(buf_index, dst);
-                        //string_len = memGetString(start_buf, buf_index, dst);
-
-                        // max_string is the maximum possible string length
-                        // (excluding the null terminated char) in the buffer
-                        max_string = ((hdr->length - sizeof(Trailer)) -
-                                      buf_index) - 1;
-                        if (max_string < 0) {
-                            getStringCorruptedCount++;
-                            return -1;
-                        }
-
-                        string_len = memGetString(
-                                start_buf, buf_index, dst, max_string);
-                        dst += string_len;
-
-                        buf_index = indexInc(buf_index, string_len + 1);
-                        eat = true;
-                        break;
-
-                        // %lld %llu %llx
-                    case 'l':
-                        i++;
-                        switch (s[i]) {
-                            case'l':
-                                i++;
-                                if (s[i] && (s[i] == 'd' || s[i] == 'i' || s[i] == 'x' || s[i] == 'u' || s[i] == 'X')) {
-                                    i++;
-                                    strncpy(format, &s[start], i-start+1);
-                                    u64 = memGetLong64(start_buf, buf_index);
-                                    buf_index = indexInc(buf_index, sizeof(u64));
-                                    dst += sprintf(dst, format, u64);
-                                    eat = true;
-                                }
-                                break;
-
-                            case 'd':
-                            case 'u':
-                            case 'i':
-                            case 'x':
-                            case 'X':
-                                i++;
-                                strncpy(format, &s[start], i-start+1);
-                                u32 = memGetInt(start_buf, buf_index);
-                                buf_index = indexInc(buf_index, sizeof(u32));
-                                dst += sprintf(dst, format, u32);
-                                eat = true;
-                                break;
-
-                            case 'f':
-                                i++;
-                                strncpy(format, &s[start], i-start+1);
-                                d = memGetDouble(start_buf, buf_index);
-                                buf_index = indexInc(buf_index, sizeof(d));
-                                dst += sprintf(dst, format, d);
-                                eat = true;
-                                break;
-
-                            default:
-                                break;
-                        }
-                        break;
-
-                    case '%':
-                        i++;
-                        dst += sprintf(dst, "%%");
-                        eat = true;
-                        break;
-
-                    default:
-                        break;
-                }
-            }
-        }
-
-        // Copy
-        if (!eat) {
-            i = start;
-            *dst = s[i];
-            dst++;
-            i++;
-        }
-        if (index + buf_index > ringBuffer_->size()) {
-            break;
-        }
-    }
-
-    *dst = 0;
-
-    if (dst > start_dst_buffer) {
-        *string_length = ((int)(dst - start_dst_buffer));
+    auto ret = stringFormat_->decodeStringFormat(s, start_buf, dst,
+                                                 hdr->length - sizeof(Trailer),
+                                                 &buf_index, string_length);
+    if (ret < 0) {
+        return ret;
     }
 
     // Adding extra trailer bytes, and return it the incoming index
     //*next_index = LOG_MEM_ALIGN(buf_index, sizeof(Trailer));
-    *next_index = LOG_MEM_ALIGN(
-            indexInc(buf_index, index) +
-            sizeof(Trailer));
+    *next_index = LOG_MEM_ALIGN(indexInc(buf_index, index) + sizeof(Trailer));
 
     // For statistic
     lastPrintedId_ = hdrid;
@@ -1035,7 +597,7 @@ void Log::printState() const {
 
 Log::Log(const char *filename, int lines, bool enableCollect, bool redirectStd)
         : marker_(LOG_MAIN_MARKER), version_(LOG_VERSION), ringBuffer_(new RingBuffer(lines)),
-          redirectStd_(redirectStd) {
+          redirectStd_(redirectStd), stringFormat_(new StringFormat()) {
     strncpy(filename_, filename, sizeof(filename_));
     fileHandle_ = createTracefile(filename, redirectStd);
     stream_ = Stream::create(fileHandle_);
--- src/lib/asynclog.h
+++ src/lib/asynclog.h
@@ -22,6 +22,7 @@ limitations under the License.
 #include <pthread.h>
 #include "ringbuffer.h"
 #include "stream.h"
+#include "stringformat.h"
 
 class Log {
 public:
@@ -118,28 +119,13 @@ private:
     Log::Trailer debugTrailer_;
     Header debugHdr_;
 
+    StringFormat *stringFormat_;
+
     char * getTraceFilename() const;
     FILE* createTracefile(const char *filename, bool redirStd);
     uint32_t getLastWrittenIndex();
     Stream *getStream() { return stream_; }
     uint32_t allocateId();
-
-    /* Memory buffer utilities */
-    uint32_t memSetByte(char *s, uint8_t u8);
-    uint32_t memSetWord(char *s, uint16_t u16);
-    uint32_t memSetInt(char *s, uint32_t u32);
-    uint32_t memSetLong64(char *s, uint64_t u64);
-    uint32_t memSetDouble(char *s, double d);
-    uint32_t memSetPtr(char *s, void *src);
-    uint32_t memSetString(char *s, const char *src);
-    uint8_t memGetByte(uint8_t *buf, uint32_t dstIndex);
-    uint16_t memGetWord(uint8_t *buf, uint32_t dstIndex);
-    uint32_t memGetInt(uint8_t *buf, uint32_t dstIndex);
-    double memGetDouble(uint8_t *buf, uint32_t dstIndex);
-    uint64_t memGetLong64(uint8_t *buf, uint32_t dstIndex);
-    void * memGetPtr(uint8_t *buf, uint32_t dstIndex);
-    uint32_t memGetString(uint8_t *buf, uint32_t buf_index, char *dst);
-    uint32_t memGetString(uint8_t *buf, uint32_t buf_index, char *dst, uint32_t maxStringLength);
 
     void setLastWrittenIndex(uint32_t index);
 
